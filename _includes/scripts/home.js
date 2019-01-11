9de6dfd38ee33be0cdfa0b43d56ba03681721f33
lisongqian <1150598307@qq.com>
2019-01-11 15:55:07 +0800

    添加百度统计代码

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -1,3 +1,11 @@
 /*(function () {
 
 })();*/
+
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement('script');
+    hm.src = 'https://hm.baidu.com/hm.js?ac13390181cda04ce0e2287162a5453a';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(hm, s);
+})();
