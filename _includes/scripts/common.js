540971c59953dd7f04156bfd2f62b6121e489885
lisongqian <1150598307@qq.com>
2019-01-11 15:58:32 +0800

    添加所有页面的百度统计代码

--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -1,7 +1,16 @@
 (function () {
-  var $root = document.getElementsByClassName('root')[0];
-  if (window.hasEvent('touchstart')) {
-    $root.dataset.isTouch = true;
-    document.addEventListener('touchstart', function(){}, false);
-  }
+    var $root = document.getElementsByClassName('root')[0];
+    if (window.hasEvent('touchstart')) {
+        $root.dataset.isTouch = true;
+        document.addEventListener('touchstart', function () {
+        }, false);
+    }
+})();
+
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement('script');
+    hm.src = 'https://hm.baidu.com/hm.js?ac13390181cda04ce0e2287162a5453a';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(hm, s);
 })();
